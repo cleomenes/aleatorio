e2e01a0ebce08c07d866b19f02c424f8f8ab5440
Cleomenes Bezerra Ferreira Junior <cleomenes2011@gmail.com>
2022-02-22 14:58:33 -0300

    add projeto

---
+++ NumRandom.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_DotNet
+{
+    static public class ListRandom
+    {
+        static Random nRand = new Random();
+        internal static List<int> Rand(int nElement, int minValue = 1, int maxValue = 100)
+        {
+            
+            List<int> listNum = new List<int>();
+
+            for(int i = 0; i < nElement; i++)
+            {
+                listNum.Add(nRand.Next(minValue, maxValue));
+            }
+
+            return listNum;
+        }
+
+        internal static List<int> RandNoRepeat(int nElement, int minValue = 1, int maxValue = 100)
+        {
+            List<int> allValues = new List<int>();
+            List<int> listRand = new List<int>();
+            
+            int iRand;
+
+            for(int i= minValue; i < maxValue; i++)
+            {
+                allValues.Add(i);
+            }
+
+            for(int i = 0; i < nElement; i++)
+            {
+                iRand = nRand.Next(0, allValues.Count);
+                listRand.Add(allValues[iRand]);
+                allValues.RemoveAt(iRand);
+            }
+
+            return listRand;
+        }
+
+
+
+        internal static string ConvertToString(List<int> listNum)
+        {
+            string sequencia = "";
+
+            foreach(var num in listNum)
+            {
+                sequencia += num + " ";
+            }
+
+            return sequencia;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using C_DotNet;
+
+List<int> lista = new List<int>(); 
+lista= ListRandom.RandNoRepeat(6, 0, 5);
+
+Console.WriteLine(ListRandom.ConvertToString(lista));
+
+
